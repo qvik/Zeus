04125725808b01423a5e0f055e775b224f74a499
Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>
2022-06-08 13:49:32 +0200

    feat: setup the view with base components and google map

--- zeus-fe/App.tsx
+++ zeus-fe/App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, View, TextInput, Dimensions } from 'react-native'
+import MapView from 'react-native-maps'
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('')
+
   return (
     <View style={styles.container}>
       <Text>Metro Exit Finder</Text>
+      <View>
+        <TextInput value={inputValue} placeholder="Adress" />
+        <Button title="Submit" />
+      </View>
+      <MapView style={styles.map} />
       <StatusBar style="auto" />
     </View>
   )
@@ -17,5 +25,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '70%',
   },
 })
--- zeus-fe/Components/ExampleComponent.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function ExampleComponent() {
-  return <div>ExampleComponent</div>
-}
