f2057ce7d84ba49be47c905d5cd4ae3a530944cc
Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>
2022-06-10 16:19:13 +0200

    feat: add directions on the map and refactor handleSubmit

--- zeus-fe/App.tsx
+++ zeus-fe/App.tsx
@@ -1,68 +1,58 @@
 import { StatusBar } from 'expo-status-bar'
 import * as React from 'react'
 import { useState } from 'react'
-import { Button, StyleSheet, Text, View, TextInput, Dimensions } from 'react-native'
+import { Button, StyleSheet, Text, View, TextInput, Alert } from 'react-native'
 import MapView from 'react-native-maps'
 import { useFonts, Poppins_700Bold } from '@expo-google-fonts/poppins'
-import axios from 'axios'
-import { travelModes, avoids } from './Utils/FejkData'
-
+import { avoids } from './Utils/MetroData'
+import MapViewDirections from 'react-native-maps-directions'
 // @ts-ignore
 import { GOOGLE_API_KEY, GOOGLE_API_BASE_URL } from '@env'
+import { initialRegion } from './Constants'
 
 export default function App() {
-  const [inputValue, setInputValue] = useState<string>('Kungsgatan 64')
-  //default mode walking
-  const [travelMode, setTravelMode] = useState(travelModes[1].name)
-  //default avoid all tolls|highways|ferries
-  const [avoid, setAvoid] = useState(avoids[0].name + '|' + avoids[1].name + '|' + avoids[2].name)
-  const [preferredExitData, setPreferredExitData] = useState({})
-  let [] = useFonts({
-    Poppins_700Bold,
-  })
+  useFonts({ Poppins_700Bold })
+  const [inputValue, setInputValue] = useState<string>('')
+  const [avoid] = useState(avoids[0].name + '|' + avoids[1].name + '|' + avoids[2].name)
+  const [preferredExit, setPreferredExit] = useState('')
+  const [destination, setDestination] = useState({ latitude: 0, longitude: 0 })
+  const [startLocation, setStartLocation] = useState({ latitude: 0, longitude: 0 })
+  const [directions, setDirections] = useState('')
+  const [error, setError] = useState('')
 
-  const initialRegion = {
-    latitude: 59.336571,
-    longitude: 18.062832,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01,
-  }
-
+  const urls = [
+    `${GOOGLE_API_BASE_URL}origin=59.336571,18.062832&destination=${inputValue}&mode=walking&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
+    `${GOOGLE_API_BASE_URL}origin=59.33633,18.062457&destination=${inputValue}&mode=walking&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
+    `${GOOGLE_API_BASE_URL}origin=59.335739,18.064088&destination=${inputValue}&mode=walking&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
+    `${GOOGLE_API_BASE_URL}origin=59.335488,18.062961&destination=${inputValue}&mode=walking&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
+  ]
   const handleSubmit = async () => {
     try {
-      // Todo: Do a map here and store the urls as array and add the remaining exits
-      await Promise.all([
-        axios.get(
-          `${GOOGLE_API_BASE_URL}origin=59.336571,18.062832&destination=${inputValue}&mode=${travelMode}&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
-        ),
-        axios.get(
-          `${GOOGLE_API_BASE_URL}origin=59.33633,18.062457&destination=${inputValue}&mode=${travelMode}&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
-        ),
-        axios.get(
-          `${GOOGLE_API_BASE_URL}origin=59.335739,18.064088&destination=${inputValue}&mode=${travelMode}&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
-        ),
-        axios.get(
-          `${GOOGLE_API_BASE_URL}origin=59.335488,18.062961&destination=${inputValue}&mode=${travelMode}&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
-        ),
-      ])
-        .then((response) => {
-          let tempValue = 13600000
-          for (let i = 0; i < response.length; i++) {
-            const legs = response[i]?.data?.routes[0]?.legs[0]
-            if (!legs) return console.log('Something wrong with setting distance variable.')
-            if (legs.distance.value <= tempValue) {
-              tempValue = legs.distance.value
-              setPreferredExitData({
-                preferredExit: legs.start_address,
-                totDistance: legs.distance.value,
-                totDuration: legs.duration.value,
-                directions: legs.steps,
-              })
-              console.log(preferredExitData, 'test')
-            }
-          }
-        })
-        .catch((err) => console.log(err))
+      const response = await Promise.all(
+        urls.map(async (url) => {
+          const resp = await fetch(url)
+          return resp.json()
+        }),
+      )
+      if (response[0].status === 'NOT_FOUND') return setError('Address not found! Please input a valid address.')
+
+      let tempValue = 13600000
+      for (let i = 0; i < response.length; i++) {
+        const legs = response[i].routes[0].legs[0]
+        if (legs.distance.value <= tempValue) {
+          tempValue = legs.distance.value
+          setPreferredExit(legs.start_address)
+          setDirections(legs.steps[0].html_instructions)
+          setStartLocation({
+            latitude: legs.steps[0].start_location.lat,
+            longitude: legs.steps[0].start_location.lng,
+          })
+          setDestination({
+            latitude: legs.steps[0].end_location.lat,
+            longitude: legs.steps[0].end_location.lng,
+          })
+        }
+      }
     } catch (error) {
       console.log(error)
     }
@@ -85,11 +75,19 @@ export default function App() {
         />
         <Button title="Submit" onPress={handleSubmit} />
       </View>
-      <MapView style={styles.map} initialRegion={initialRegion} />
+      <MapView style={styles.map} initialRegion={initialRegion}>
+        <MapViewDirections mode="WALKING" origin={startLocation} destination={destination} apikey={GOOGLE_API_KEY} />
+      </MapView>
+      {preferredExit ? (
+        <View>
+          <Text>Take the exit: {preferredExit}</Text>
+        </View>
+      ) : null}
       <StatusBar style="auto" />
     </View>
   )
 }
+
 // Todo: Add correct CSS from figma
 const styles = StyleSheet.create({
   container: {
@@ -109,7 +107,6 @@ const styles = StyleSheet.create({
   input: {
     marginRight: '8px',
     border: '1px solid',
-
     width: '241px',
     height: '41px',
     filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
---
+++ zeus-fe/Constants/index.ts
@@ -0,0 +1,6 @@
+export const initialRegion = {
+  latitude: 59.336571,
+  longitude: 18.062832,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+}
