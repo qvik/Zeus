c131b1ac0e3360eb593d4296ff7a46dd494d1887
cocoons <signupkonto@gmail.com>
2022-06-13 10:38:44 +0200

    feat: dynamic data and urls and reusables (#3)
    * dynamic data and urls and reusables..Needs some cleaning quite a lot of debugs
    
    * fix: cleanup
    
    Co-authored-by: Alexander.silverohrt <alexander.silverohrt@qvik.fi>
    Co-authored-by: Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>

--- zeus-fe/App.tsx
+++ zeus-fe/App.tsx
@@ -1,18 +1,18 @@
 import { StatusBar } from 'expo-status-bar'
 import * as React from 'react'
 import { useState } from 'react'
-import { Button, StyleSheet, Text, View, TextInput, Alert } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import MapView from 'react-native-maps'
 import { useFonts, Poppins_700Bold } from '@expo-google-fonts/poppins'
-import { avoids } from './Utils/MetroData'
+import { avoids, metroExits, travelModes, initialRegion } from './Constants'
 import MapViewDirections from 'react-native-maps-directions'
+import { DestinationInput } from './Components/DestinationInput'
 // @ts-ignore
 import { GOOGLE_API_KEY, GOOGLE_API_BASE_URL } from '@env'
-import { initialRegion } from './Constants'
 
 export default function App() {
   useFonts({ Poppins_700Bold })
-  const [inputValue, setInputValue] = useState<string>('')
+  const [selectedDestination, setSelectedDestination] = useState<string>('')
   const [avoid] = useState(avoids[0].name + '|' + avoids[1].name + '|' + avoids[2].name)
   const [preferredExit, setPreferredExit] = useState('')
   const [destination, setDestination] = useState({ latitude: 0, longitude: 0 })
@@ -20,13 +20,14 @@ export default function App() {
   const [directions, setDirections] = useState('')
   const [error, setError] = useState('')
 
-  const urls = [
-    `${GOOGLE_API_BASE_URL}origin=59.336571,18.062832&destination=${inputValue}&mode=walking&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
-    `${GOOGLE_API_BASE_URL}origin=59.33633,18.062457&destination=${inputValue}&mode=walking&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
-    `${GOOGLE_API_BASE_URL}origin=59.335739,18.064088&destination=${inputValue}&mode=walking&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
-    `${GOOGLE_API_BASE_URL}origin=59.335488,18.062961&destination=${inputValue}&mode=walking&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
-  ]
   const handleSubmit = async () => {
+    let urls: string[] = []
+    metroExits.forEach((it) => {
+      urls = [
+        ...urls,
+        `${GOOGLE_API_BASE_URL}origin=${it.latitude},${it.longitude}&destination=${selectedDestination}&mode=${travelModes[1].name}&avoid=${avoid}&key=${GOOGLE_API_KEY}`,
+      ]
+    })
     try {
       const response = await Promise.all(
         urls.map(async (url) => {
@@ -34,6 +35,7 @@ export default function App() {
           return resp.json()
         }),
       )
+      console.log(`response: is: ${JSON.stringify(response[0])}`)
       if (response[0].status === 'NOT_FOUND') return setError('Address not found! Please input a valid address.')
 
       let tempValue = 13600000
@@ -42,14 +44,14 @@ export default function App() {
         if (legs.distance.value <= tempValue) {
           tempValue = legs.distance.value
           setPreferredExit(legs.start_address)
-          setDirections(legs.steps[0].html_instructions)
+          setDirections(legs.steps)
           setStartLocation({
-            latitude: legs.steps[0].start_location.lat,
-            longitude: legs.steps[0].start_location.lng,
+            latitude: legs.start_location.lat,
+            longitude: legs.start_location.lng,
           })
           setDestination({
-            latitude: legs.steps[0].end_location.lat,
-            longitude: legs.steps[0].end_location.lng,
+            latitude: legs.end_location.lat,
+            longitude: legs.end_location.lng,
           })
         }
       }
@@ -65,24 +67,23 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}>Metro Exits</Text>
-      <Text style={styles.finderText}>FINDER</Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          value={inputValue}
-          onChangeText={(value: any) => setInputValue(value)}
-          placeholder="Adress"
-        />
-        <Button title="Submit" onPress={handleSubmit} />
-      </View>
+      <DestinationInput
+        selectedDestination={selectedDestination}
+        setSelectedDestination={setSelectedDestination}
+        handleSubmit={handleSubmit}
+      />
       <MapView style={styles.map} initialRegion={initialRegion}>
         <MapViewDirections mode="WALKING" origin={startLocation} destination={destination} apikey={GOOGLE_API_KEY} />
       </MapView>
       {preferredExit ? (
-        <View>
-          <Text>Take the exit: {preferredExit}</Text>
-        </View>
-      ) : null}
+        <>
+          <View>
+            <Text style={{ marginTop: 6, height: 30 }}>Take the exit: {preferredExit}</Text>
+          </View>
+        </>
+      ) : (
+        <></>
+      )}
       <StatusBar style="auto" />
     </View>
   )
@@ -91,34 +92,27 @@ export default function App() {
 // Todo: Add correct CSS from figma
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
     backgroundColor: '#D9D9D9',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20,
   },
   map: {
     width: '100%',
-    height: '70%',
-  },
-  inputContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  input: {
-    marginRight: '8px',
-    border: '1px solid',
-    width: '241px',
-    height: '41px',
-    filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+    height: '75%',
   },
   titleText: {
     fontFamily: 'Poppins_700Bold',
     fontWeight: '700',
-    fontSize: 38,
+    fontSize: 30,
+    marginBottom: 0,
   },
   finderText: {
     fontFamily: 'Poppins_700Bold',
     fontWeight: '700',
-    fontSize: 48,
+    fontSize: 40,
+    marginTop: 0,
+    paddingTop: 0,
   },
 })
---
+++ zeus-fe/Components/DestinationInput.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { StyleSheet, Text, View, TextInput, Alert, Pressable } from 'react-native'
+
+export const DestinationInput = (props: any) => {
+  return (
+    <>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={props.selectedDestination}
+          onChangeText={(value: any) => props.setSelectedDestination(value)}
+          placeholder="Input destination"
+        />
+        <Pressable style={styles.button} onPress={props.handleSubmit}>
+          <Text style={styles.buttonText}> Submit </Text>
+        </Pressable>
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  input: {
+    marginBottom: 10,
+    marginTop: 13,
+    marginRight: '8px',
+    width: '241px',
+    height: '41px',
+  },
+  button: {
+    marginBottom: 10,
+    marginTop: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontFamily: 'Poppins_700Bold',
+    fontWeight: '700',
+    fontSize: 12,
+    color: 'blue',
+    borderRightWidth: 3,
+  },
+})
